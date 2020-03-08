04439262596ddf44b0c14fdc527321722ab502e8
ichu4321 <ianchu@umich.edu>
2020-03-08 18:29:50 -0400

    Initial Commit

---
+++ classes.js
@@ -0,0 +1,251 @@
+class Object {
+	constructor(x, y, sprite, visible) {
+		this.x = x;
+		this.y = y;
+		this.sprite = sprite;
+		this.show = visible; // true or false bool
+	}
+	// draws a scale-rectified image
+	draw() {
+		if (this.show) {
+			scaledDraw(this.sprite, this.x, this.y);
+		}	
+	}
+}
+class Player extends Object {
+	constructor(x, y, sprite, visible, lerpSpeed) {
+		super(x, y, sprite, visible);
+		this.lerpSpeed = lerpSpeed;
+	}
+	update(dt) {
+		// check stun timer
+		if (stun_timer <= 0) {
+			let diffx = (mousex - this.sprite.width / 2) - this.x;
+			let diffy = (mousey - this.sprite.height / 2) - this.y;
+			this.x += this.lerpSpeed * diffx;
+			this.y += this.lerpSpeed * diffy;
+		}
+		// console.log("Player Pos: " + this.x + ", " + this.y);
+	}
+}
+
+// collideables
+var col_list = [];
+class Collider extends Object{
+	constructor(x, y, sprite, visible, radius) {
+		super(x,y,sprite,visible);
+		this.radius = radius;
+		col_list.push(this);
+	}
+	update(dt) {
+		// empty
+	}
+}
+class Enemy extends Collider {
+	constructor(x, y, sprite, visible, radius, targetx, targety, lerpSpeed) {
+		super(x, y, sprite, visible, radius);
+		this.targetx = targetx;
+		this.targety = targety;
+		this.lerpSpeed = lerpSpeed;
+		this.shoot_timer = randFloat(1.0, 3.0);
+	}
+	// movement // actually, let the ball destroy the enemy
+	update(dt) {
+		// only do a y lerp for now
+		let diffy = this.targety - this.y;
+		this.y += this.lerpSpeed * diffy;
+
+		// shoot
+		this.shoot_timer -= dt;
+		if (this.shoot_timer <= 0) {
+			this.shoot_timer = randFloat(1.0, 3.0);
+			let temp = new Hazard(this.x + this.radius, this.y + this.radius, enemy_bullet_sprite, true, 8, 100);
+		}
+	}
+}
+hazard_list = [];
+class Hazard extends Object {
+	constructor(x, y, sprite, visible, radius, vely) {
+		super(x,y,sprite,visible);
+		this.radius = radius;
+		this.vely = vely;
+		hazard_list.push(this);
+	}
+	update(dt) {
+		this.y += dt * this.vely;
+		// do garbage collection in main loop's update
+	}
+}
+
+// really just a class with velocity physics
+class Particle extends Object{
+	constructor(x, y, sprite, visible, velx, vely) {
+		super(x,y,sprite,visible);
+		this.velx = velx;
+		this.vely = vely;
+	}
+	// moves the particle
+	update(dt) {
+		this.x += this.velx * dt;
+		this.y += this.vely * dt;
+	}
+}
+
+class Ball extends Particle {
+	constructor(x, y, sprite, visible, velx, vely, radius) {
+		super(x,y,sprite,visible,velx,vely);
+		this.radius = radius;
+		this.speed = this.magnitude(this.velx, this.vely);
+		console.log("Init Ball: " + this.radius + ", " + this.speed);
+	}
+	magnitude(x, y) {
+		return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+	}
+	dist(otherx, othery) {
+		let diffx = otherx - this.x;
+		let diffy = othery - this.y;
+		return Math.sqrt(Math.pow(diffx, 2) + Math.pow(diffy, 2));
+	}
+	bounce(x, y) {
+		// get angle
+		let diffx = this.x - x;
+		let diffy = this.y - y;
+		let angle = Math.atan2(diffy, diffx);
+
+		// change velx, vely but maintain speed
+		this.velx = this.speed * Math.cos(angle);
+		this.vely = this.speed * Math.sin(angle);
+	}
+	wall_bounce() {
+		// check for wall bounce
+		// left,right (remember: x,y is the top_left of the sprite)
+		if (this.x < 0 || this.x + this.radius * 2 > canvas.scrollWidth) {
+			this.velx = -this.velx;
+			// console.log("Side Wall Bounce: " + this.x + "|" + canvas.scrollWidth);
+		}
+		// up,down
+		if (this.y < 0 || this.y + this.radius * 2 > canvas.scrollHeight) {
+			this.vely = -this.vely;
+			// console.log("Updown Wall Bounce: " + this.y + "|" + canvas.scrollHeight);
+		}
+		// bouncing off of the health indicator
+		if (this.y + this.radius * 2 >= health_indicator.y) {
+			this.vely = -this.vely;
+			// update health indicator
+			health_index += 1;
+			if (health_index < back.length) {
+				health_indicator.sprite = back[health_index];
+			}
+			else {
+				// gameover
+				set_gameover();
+			}
+		}
+	}
+	collision() {
+		// check col_list
+		for (a = 0; a < col_list.length; a++) {
+			// check collision distance
+			let otherx = col_list[a].x;
+			let othery = col_list[a].y;
+			let min_dist = this.radius + col_list[a].radius;
+			if (this.dist(otherx, othery) <= min_dist) {
+				// bounce off object
+				this.bounce(otherx, othery);
+				// destroy if enemy
+				if (col_list[a] instanceof Enemy) {
+					delete col_list[a];
+					col_list.splice(a,1);
+					a - 1;
+					score += 1;
+					// console.log("Destroyed an Enemy");
+				}
+			}
+		}
+	}
+	update(dt) {
+		// check for collisions
+		this.collision();
+		this.wall_bounce();
+
+		// use particle's update method
+		super.update(dt);
+	}
+}
+class Shield extends Collider {
+	constructor(x, y, sprite, visible, radius) {
+		super(x, y, sprite, visible, radius);
+	}
+	// dist to other
+	dist(otherx, othery) {
+		let diffx = otherx - this.x;
+		let diffy = othery - this.y;
+		return Math.sqrt(Math.pow(diffx, 2) + Math.pow(diffy, 2));
+	}
+	// hazard collision
+	hazard_collision() {
+		for (a = 0; a < hazard_list.length; a++) {
+			let min_dist = this.radius + hazard_list[a].radius;
+			if (this.dist(hazard_list[a].x, hazard_list[a].y) <= min_dist) {
+				// destroy bullet
+				delete hazard_list[a];
+				hazard_list.splice(a,1);
+				a - 1;
+
+				// get stunned if not already stunned
+				if (stun_timer < -0.5) {
+					stun_timer = 1.0;
+				}
+			}
+		}
+	}
+	// movement // actually, let the ball destroy the enemy
+	update(dt) {
+		// check for collisions with hazards
+		this.hazard_collision();
+
+		// check for stunner
+		if (stun_timer > 0) {
+			// change to stun sprite
+			this.sprite = stun_sprite;
+		}
+		else {
+			this.sprite = shield_sprite;
+		}
+		stun_timer -= dt;
+
+		// follow player
+		this.x = player.x + player.sprite.width / 2 - this.sprite.width / 2;
+		this.y = player.y + player.sprite.height / 2 - this.sprite.height / 2;
+
+		// use parent's update
+		super.update(dt);
+	}
+}
+
+class ParallaxLayer {
+	constructor(low, high) {
+		this.range = [low, high];
+		this.elems = [];
+	}
+	addElem(x, y, sprite) {
+		this.elems.push(new Particle(x, y,sprite, true, 0, randInt(this.range[0], this.range[1])));
+	}
+	update(dt) {
+		for (a = 0; a < this.elems.length; a++) {
+			this.elems[a].update(dt);
+			// check if out of bounds
+			if (this.elems[a].y > canvas.scrollHeight) {
+				// reset
+				this.elems[a].y = 0;
+				this.elems[a].x = randInt(0,canvas.scrollWidth);
+			}
+		}
+	}
+	draw() {
+		// draw each particle
+		for (a = 0; a < this.elems.length; a++) {
+			this.elems[a].draw();
+		}
+	}
+}
---
+++ main.js
@@ -0,0 +1,224 @@
+// declare references to main canvas
+var canvas = document.getElementById("game_window");
+var context = canvas.getContext("2d");
+context.font = "16px Arial";
+var scalex = canvas.width / canvas.scrollWidth;
+var scaley = canvas.height / canvas.scrollHeight;
+// canvas.style.cursor = "none";
+
+// get game resources
+var player_sprite = new Image();
+player_sprite.src = "ship.png";
+var shield_sprite = new Image();
+shield_sprite.src = "shield.png";
+var stun_sprite = new Image();
+stun_sprite.src = "stun_shield.png";
+var star_sprite = new Image();
+star_sprite.src = "star.png";
+var ball_sprite = new Image();
+ball_sprite.src = "ball.png";
+var enemy_bullet_sprite = new Image();
+enemy_bullet_sprite.src = "enemy_bullet.png";
+
+// enemy sprites
+var enemy1_sprite = new Image();
+enemy1_sprite.src = "enemy1.png";
+
+// backboard sprites
+var back = [];
+for (a = 0; a < 3; a++) {
+	back.push(new Image());
+}
+back[0].src = "green_box.png";
+back[1].src = "yellow_box.png";
+back[2].src = "red_box.png";
+var health_index = 0;
+var health_indicator = new Object(0, canvas.scrollHeight - 50, back[health_index], true);
+// WARNING: sprite width and height are 0 for some reason
+// this is because the image hasn't actually loaded yet
+// gonna need some js voodoo to make this all work
+// screw it, just hardcode the sprite width
+
+// background parallax layer
+var bg_layer = new ParallaxLayer(100, 250);
+
+// player object
+// var player = new Collider(0,0, player_sprite, true, 16);
+var player = new Player(0,0, player_sprite, true, .15);
+var shield = new Shield(0,0, shield_sprite, true, 16);
+
+// ball object
+var ball = new Ball(canvas.scrollWidth/2, 100, ball_sprite, true, 200, 150, 16);
+// console.log("Init Ball Pos: " + ball.x + ", " + ball.y);
+
+// enemy ships
+var spawn_range = [2.0, 3.0]; // seconds
+var next_spawn = 1.0;
+
+// track game time
+var fps = 30; // might not use this
+var last_time = new Date();
+
+// score
+var score = 0;
+
+// gameover setting
+var gameover = false;
+
+// stun stuff
+var stun_timer = 0;
+
+// register mouse position and move through the update function
+var mousex = canvas.scrollWidth / 2;
+var mousey = canvas.scrollHeight * .75;
+
+// init onload
+window.onload = init();
+
+// get mouse events
+function init() {
+	console.log("Initializing...");
+	canvas.addEventListener("pointermove", canvasMove, false);
+
+	// fill parallax layer
+	for (a=0; a<20; a++) {
+		randx = randInt(0, canvas.scrollWidth);
+		randy = randInt(0, canvas.scrollHeight);
+		bg_layer.addElem(randx, randy, star_sprite);
+	}
+}
+
+// main loop
+var frame_counter = 0;
+function mainLoop() {
+	// only do things if not in gameover mode
+	if (!gameover) {
+		// get time diff
+		let curr_time = new Date();
+		let dt = curr_time - last_time; // returns milliseconds
+		dt /= 1000; // in seconds
+		last_time = curr_time;
+		// run main game functions
+		spawn(dt);
+		update(dt);
+		render();
+		// console.log("Frame: " + frame_counter++);
+	}
+	else {
+		// set black
+		context.fillStyle = "black";
+		context.fillRect(0,0,canvas.width,canvas.height);
+		// show game over
+		context.fillStyle = "red";
+		context.textAlign = "center";
+		context.fillText("Final Score: " + score, canvas.width / 2, canvas.height / 2);
+	}
+	requestAnimationFrame(mainLoop);
+}
+
+// Start things off
+requestAnimationFrame(mainLoop);
+
+// spawn new enemies
+function spawn(dt) {
+	// countdown timer
+	next_spawn -= dt;
+	if (next_spawn <= 0) {
+		next_spawn = randFloat(spawn_range[0], spawn_range[1]);
+		// spawn new enemy
+		let sprite = enemy1_sprite;
+		let rx = randInt(0, canvas.scrollWidth - sprite.width * 2);
+		let ry = randInt(sprite.height*2, canvas.scrollHeight / 3);
+		let new_enemy = new Enemy(rx, -16, sprite, true, 16, 0, ry, .15);
+		// console.log("Spawned new enemy: " + rx + ", " + ry);
+	}
+}
+
+// update everything
+function update(dt) {
+	// update
+	bg_layer.update(dt);
+	for (a = 0; a < col_list.length; a++) {
+		col_list[a].update(dt);
+	}
+	player.update(dt);
+	shield.update(dt);
+	ball.update(dt);
+	for (a = 0; a <hazard_list.length; a++) {
+		hazard_list[a].update(dt);
+		// destroy if off the canvas
+		if (hazard_list[a].y > canvas.scrollHeight) {
+			delete hazard_list[a];
+			hazard_list.splice(a,1);
+			a - 1;
+		}
+	}
+}
+
+// draw things in order
+function render() {
+	// draw back to front
+	context.fillStyle = "black";
+	context.fillRect(0,0,canvas.width,canvas.height);
+	bg_layer.draw();
+	health_indicator.draw();
+	player.draw();
+	for (a = 0; a < col_list.length; a++) {
+		col_list[a].draw();
+	}
+	ball.draw();
+	for (a = 0; a < hazard_list.length; a++) {
+		hazard_list[a].draw();
+	}
+	context.fillStyle = "white";
+	context.textAlign = "start";
+	context.fillText("Score: " + score, 20, 20);
+}
+
+// react to movement
+function canvasMove(event) {
+	// get mouse position
+	var x = -1;
+	var y = -1;
+	// check browser specifics
+	if (event.layerX || event.layerX == 0) {
+		// firefox
+		x = event.layerX;
+		y = event.layerY;
+	}
+	else if (event.offsetX || event.offsetX == 0) {
+		// opera
+		x = event.offsetX;
+		y = event.offsetY;
+	}
+
+	// update player position // if not stunned
+	mousex = x;
+	mousey = y;
+}
+
+// draw a scale rectified sprite on the screen
+function scaledDraw(sprite, x, y) {
+	context.drawImage(sprite, Math.round(x * scalex), Math.round(y * scaley), sprite.width * scalex, sprite.height * scaley);
+}
+
+// because javascript lets you put the wrong stuff into a function
+// and doesn't throw any errors, it just ignores them (WHY THOUGH?)
+function randFloat(low, high) {
+	return (Math.random() * high) + low;
+}
+function randInt(low, high) {
+	return Math.round(randFloat(low, high));
+}
+
+// a blocking sleep function
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// swaps to gameover screen
+function set_gameover() {
+	// gameover = true;
+	console.log("Set Gameover");
+	// not doing anything here anymore
+}
